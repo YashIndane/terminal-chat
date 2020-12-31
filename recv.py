46a514569eba2cf71dec0cb9311485f8411ba4f8
Yash Indane <yashindane46@gmail.com>
2020-12-31 15:19:52 +0530

    Update recv.py

--- recv.py
+++ recv.py
@@ -4,14 +4,19 @@ import os
 import datetime
 
 print('')
-sc = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
-server_ip = input('enter your ip : ')
-server_port = int(input('enter your port number : '))
-sc.bind((server_ip , server_port))
 
-sc1 = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
-sip = input('enter other end ip : ')
-spo = int(input('enter other end port number : '))
+family = socket.AF_INET
+protocol = socket.SOCK_DGRAM
+
+socket1 = socket.socket(family , protocol)
+server_ip1 = input('enter your ip : ')
+server_port1 = int(input('enter your port number : '))
+socket1.bind((server_ip1 , server_port1))
+
+socket2 = socket.socket(family , protocol)
+server_ip2 = input('enter other end ip : ')
+server_port2 = int(input('enter other end port number : '))
+
 print('')
 
 def time():
@@ -22,8 +27,9 @@ def time():
 
 def receive():
   while 1:
-
-    msg = sc.recvfrom(1024)
+    
+    buffer_size = 1024
+    msg = socket1.recvfrom(buffer_sizeo)
     os.system('tput setaf 32')
     print('\U0001F464' + ' ' + msg[0].decode())
     os.system('tput setaf 7')
@@ -34,7 +40,7 @@ def send():
   while 1:
 
     msg = input()
-    sc1.sendto(msg.encode() , (sip , spo))
+    socket2.sendto(msg.encode() , (server_ip2 , server_port2))
     t_ = time()
     print('__'*(36) + t_ + '\n')
 
